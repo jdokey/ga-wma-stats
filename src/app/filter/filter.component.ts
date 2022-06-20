9b6a2381a337e0d8842609ed50efe4b9bb6c5ed6
Dave Okey <david.j.okey@gmail.com>
2022-06-20 12:40:38 -0400

    New filters and clear btns

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -6,6 +6,8 @@ import { WmaService } from './../data.services/wma.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { HuntFilter } from '../model';
+import { MatSliderChange } from '@angular/material/slider';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-filter',
@@ -32,7 +34,9 @@ export class FilterComponent implements OnInit {
       wma: [null as string | null],
       season: [null as string | null],
       weapon: [null as string | null],
-      successRate: [null as string | null]
+      successRate: [null as string | null],
+      quota: [null as string | null],
+      isCheckIn: [null as string | null]
     });
 
     this.filterForm.valueChanges.subscribe(filterValues => {
@@ -42,12 +46,24 @@ export class FilterComponent implements OnInit {
   }
 
   filtersChanged(value: HuntFilter) {
+    console.log('filtersChanged: ', value);
     this._filterService.selectedFiltersChanged(value);
   }
 
-  clearFilters() {
+  clearFilter(name: string): void {
+    this.filterForm.controls[name].reset(null, { emitEvent: false });
+    this.filtersChanged(this.filterForm.value);
+  }
+
+  clearFilters(): void {
     this.filtersChanged({});
     this.filterForm.reset({}, { emitEvent: false });
+  }
+
+  toggleChange(ctrl: string, event: MatSlideToggleChange) {
+    if (!event.checked) {
+      this.filterForm.controls[ctrl].setValue(null);
+    }
   }
 
 }
