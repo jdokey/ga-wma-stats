f3e971ff0447ea69b7cc7448132de47c1befc533
Dave Okey <david.j.okey@gmail.com>
2023-05-13 21:32:18 -0400

    Moved huntType data to static property

--- src/app/pipes/hunt-type.pipe.ts
+++ src/app/pipes/hunt-type.pipe.ts
@@ -1,4 +1,3 @@
-import { HuntTypeData } from '../data/huntTypeData';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -7,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HuntTypePipe implements PipeTransform {
 
   transform(id: number): string | undefined {
-    return HuntTypeData.huntTypeData.find(value => value.id === id)?.descr;
+    const huntTypes = [{"_id":"62b0c3a97a044da7bc9e2830","name":"QBO","id":6},{"_id":"62b0c3a97a044da7bc9e2832","name":"ESL3D","id":8},{"_id":"62b0c3a97a044da7bc9e2831","name":"QBALD","id":7},{"_id":"62b0c3a97a044da7bc9e2839","name":"Anterless","id":15},{"_id":"62b0c3a97a044da7bc9e282e","name":"ESL2D","id":4},{"_id":"62b0c3a97a044da7bc9e282c","name":"QBA","id":2},{"_id":"62b0c3a97a044da7bc9e282b","name":"ES","id":1},{"_id":"62b0c3a97a044da7bc9e2834","name":"Hog/Dog","id":10},{"_id":"62b0c3a97a044da7bc9e2833","name":"Hog Only","id":9},{"_id":"62b0c3a97a044da7bc9e2838","name":"Antlerless","id":14},{"_id":"62b0c3a97a044da7bc9e282f","name":"ESLD","id":5},{"_id":"62b0c3a97a044da7bc9e282d","name":"BO","id":3},{"_id":"62b0c3a97a044da7bc9e2835","name":"QBAL2D","id":11},{"_id":"62b0c3a97a044da7bc9e2836","name":"AO","id":12},{"_id":"62b0c3a97a044da7bc9e2837","name":"QBAL6D","id":13}];
+    return huntTypes.find(value => value.id === id)?.name;
   }
 
 }
